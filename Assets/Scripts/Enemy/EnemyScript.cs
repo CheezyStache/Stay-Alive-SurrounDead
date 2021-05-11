7e2adf1a1e7cb12b5769b1b5173bc285fed58abe
Oleksyi <cutpaks@ukr.net>
2021-05-11 23:35:17 +0300

    Boximon collider fix, run animation

--- Assets/Scripts/Enemy/EnemyScript.cs
+++ Assets/Scripts/Enemy/EnemyScript.cs
@@ -9,16 +9,22 @@ public class EnemyScript : MonoBehaviour
     [HideInInspector] public Transform PlayerTransform { get; set; }
 
     private Rigidbody _rigidbody;
+    private Animator _animator;
+
+    private const string RunForwardAnimation = "Run Forward";
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
         if (PlayerTransform == null)
             return;
+
+        _animator.SetBool(RunForwardAnimation, true);
 
         var step = Time.deltaTime * _speed;
         _rigidbody.MovePosition(Vector3.MoveTowards(transform.position, PlayerTransform.position, step));
