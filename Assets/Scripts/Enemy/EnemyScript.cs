0e8596bd88dfd43265932cb3d3e3bb121ac867a0
Oleksyi <cutpaks@ukr.net>
2021-05-19 23:19:43 +0300

    Mob dies on collision

--- Assets/Scripts/Enemy/EnemyScript.cs
+++ Assets/Scripts/Enemy/EnemyScript.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private float _dieDelay;
+    [SerializeField] private float _dieFallSpeed;
+    [SerializeField] private float _dieFallY;
 
     [HideInInspector] public Transform PlayerTransform { get; set; }
 
@@ -12,9 +15,11 @@ public class EnemyScript : MonoBehaviour
     private Animator _animator;
 
     private bool isRunning = true;
+    private bool died = false;
 
     private const string RunForwardAnimation = "Run Forward";
     private const string AttackAnimation = "Attack 01";
+    private const string DieAnimation = "Die";
 
     void Start()
     {
@@ -32,6 +37,9 @@ public class EnemyScript : MonoBehaviour
 
     void OnTriggerStay(Collider collider)
     {
+        if(died)
+            return;
+
         if (collider.gameObject.tag != "Player")
             return;
 
@@ -41,11 +49,23 @@ public class EnemyScript : MonoBehaviour
 
     void OnTriggerExit(Collider collider)
     {
+        if (died)
+            return;
+
         if (collider.gameObject.tag != "Player")
             return;
 
         isRunning = true;
         ChangeAnimation(RunForwardAnimation);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (died)
+            return;
+
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Hammer"))
+            Die();
     }
 
 
@@ -64,5 +84,34 @@ public class EnemyScript : MonoBehaviour
             _animator.SetBool(parameter.name, false);
 
         _animator.SetBool(nextAnimation, true);
+    }
+
+    private void Die()
+    {
+        isRunning = false;
+        died = true;
+        ChangeAnimation(DieAnimation);
+        StartCoroutine(DieFall());
+    }
+
+    private IEnumerator DieFall()
+    {
+        yield return new WaitForSeconds(_dieDelay);
+
+        _rigidbody.isKinematic = true;
+
+        var time = 0f;
+        var fallTime = (transform.position.y - _dieFallY) / _dieFallSpeed;
+
+        while (time < fallTime)
+        {
+            time += Time.deltaTime;
+
+            transform.Translate(Vector3.down * _dieFallSpeed * Time.deltaTime);
+
+            yield return null;
+        }
+
+        Destroy(gameObject);
     }
 }
